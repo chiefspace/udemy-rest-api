858202509e18b882f1326545c72757cd26cc2a86
Ben Altieri <chiefspace@gmail.com>
2017-05-26 10:18:10 -0400

    pushing code up to time marker 13.09 in Section 6, Lecture 73

--- udemy_rest_api_section6/code/app.py
+++ udemy_rest_api_section6/code/app.py
@@ -3,8 +3,8 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from user import RegisterUser
-from item import Item, ItemList
+from resources.user import RegisterUser
+from resources.item import Item, ItemList
 
 app = Flask(__name__)
 app.secret_key = 'P@ssw0rd'
