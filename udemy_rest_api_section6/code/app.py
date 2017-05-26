667b5c8215414d82d91891339bede08dad0d9ae0
Ben Altieri <chiefspace@gmail.com>
2017-05-26 13:22:15 -0400

    Begun Flask_SQLAlchemy implementation

--- udemy_rest_api_section6/code/app.py
+++ udemy_rest_api_section6/code/app.py
@@ -17,4 +17,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(RegisterUser, '/register')
 
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(host='0.0.0.0', port=5000, debug=True)
