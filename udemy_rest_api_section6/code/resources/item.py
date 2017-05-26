c609651d0fd7fac307cc331135547265daca8e26
Ben Altieri <chiefspace@gmail.com>
2017-05-26 11:43:21 -0400

    Changed return updated_item to return updated_item.json() for the return statement in the put method of the updated_item resource

--- udemy_rest_api_section6/code/resources/item.py
+++ udemy_rest_api_section6/code/resources/item.py
@@ -61,7 +61,7 @@ class Item(Resource):
                 updated_item.update()
             except:
                 return {"message": "An error occurred while updating the item."}, 500
-        return updated_item      
+        return updated_item.json()
 
 
 class ItemList(Resource):
