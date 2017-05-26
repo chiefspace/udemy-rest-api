cbd83d311e12e89e992d0d03ad7ae80c55e96f8b
Ben Altieri <chiefspace@gmail.com>
2017-05-26 14:04:07 -0400

    added SQLAlchemy table model to the item model

--- udemy_rest_api_section6/code/models/item.py
+++ udemy_rest_api_section6/code/models/item.py
@@ -2,6 +2,12 @@ import sqlite3
 import db from db
 
 class ItemModel(db.Model):
+    __tablename__ = 'items'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    price = db.Column(db.Float(precision=2))
+    
     def __init__(self, name, price):
         self.name = name
         self.price = price
