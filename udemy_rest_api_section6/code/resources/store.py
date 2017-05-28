18a9736461639897d0928297d7e7461628ecfaca
Ben Altieri <chiefspace@gmail.com>
2017-05-28 11:45:52 -0400

    wrote Store resource with list comprehension for return value for get StoreList resource

--- udemy_rest_api_section6/code/resources/store.py
+++ udemy_rest_api_section6/code/resources/store.py
@@ -3,14 +3,31 @@ from models.store import StoreModel
 
 class Store(Resource):
     def get(self, name):
-        pass
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json()
+        return {'message': 'Store not found'}, 404
       
     def post(self, name):
-        pass
+        if StoreModel.find_by_name(name):
+            return {'message': "A store with name '{}' already exists.".format(name)}, 400
+
+        store = StoreModel(name)
+        try:
+            store.save_to_db()
+        except:
+            return {"message": "An error occurred creating the store."}, 500
+      
+        return store.json(), 201
       
     def delete(self, name):
-        pass
+        store = StoreModel.find_by_name(name)
+        if store:
+            store.delete_from_db()
+
+        return {'message': 'Store deleted'}
       
 
 class StoreList(Resource):
-    pass
+    def get(self):
+        return {'stores': [store.json() for store in StoreModel.query.all()]}
