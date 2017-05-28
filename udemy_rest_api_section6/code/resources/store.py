644f1dc35a00614d04cd8be36eb46323dc86d516
Ben Altieri <chiefspace@gmail.com>
2017-05-28 11:16:41 -0400

    created Store resource template

---
+++ udemy_rest_api_section6/code/resources/store.py
@@ -0,0 +1,16 @@
+from Flask_RESTful import Resource
+from models.store import StoreModel
+
+class Store(Resource):
+    def get(self, name):
+        pass
+      
+    def post(self, name):
+        pass
+      
+    def delete(self, name):
+        pass
+      
+
+class StoreList(Resource):
+    pass
